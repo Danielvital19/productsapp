d4e48dfd600666c2bc402c3102ec47f9a00ff92a
Danielvital19 <45982175+Danielvital19@users.noreply.github.com>
2022-08-04 00:32:18 -0500

    clean SearchBar test file

--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -5,13 +5,6 @@ import '@testing-library/jest-dom/extend-expect'
 import { BrowserRouter } from 'react-router-dom'
 import { screen } from '@testing-library/react'
 
-// const mockedUsedNavigate = jest.fn();
-
-// jest.mock('react-router-dom', () => ({
-//    ...jest.requireActual('react-router-dom'),
-//   useNavigate: () => mockedUsedNavigate,
-// }));
-
 describe('SearchBar component', () => {
     it('should render the component onto the screen', () => {
         render(<BrowserRouter><SearchBar /></BrowserRouter>);
@@ -26,44 +19,6 @@ describe('Input value', () => {
       const { queryByPlaceholderText } = render(<BrowserRouter><SearchBar setSearch={handleSearch}/></BrowserRouter>)
       const searchInput = queryByPlaceholderText('Nunca dejes de buscar')
       fireEvent.change(searchInput, { target: { value: 'test' } })
-
       expect(searchInput.value).toBe('test')
     })
-  })
-
-  describe('Make search', () => {
-    // it('handleSearch function should not be callen when making an empty search', () => {
-    //     // render(<BrowserRouter><SearchBar /></BrowserRouter>);
-
-    //     const wrapper = shallow((<SearchBar />));
-
-    //     wrapper.find('search-button').simulate('click');
-
-    //     // const input = screen.getByTestId('search-input');
-    //     // const button = screen.getByTestId('search-button');
-    //     // var spy = jest.spyOn(SearchBar.prototype, 'handleSearch');
-
-    //     // console.log(button)
-
-    //     // fireEvent.change(input, {target: {value: ''}});
-    //     // fireEvent.click(button);
-
-    //     // expect(spy).toHaveBeenCalledTimes(1);
-    // });
-
-    // it('searches pushes search route with search query', () => {
-    //     const container = mount(
-    //         <SearchBar router={{ query: {}, route: '/search' }} />
-    //     );
-
-    //     act(() => {
-    //         container.find('Form').props().onChange({
-    //             values: { search: 'test val' }
-    //         });
-    //     });
-
-    //     expect(Router.router.push).toHaveBeenCalledTimes(1);
-    //     expect(Router.router.push).toHaveBeenCalledWith('/search?q=test val');
-    // });
-
   })
